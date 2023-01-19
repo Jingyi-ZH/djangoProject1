c3d9cadafd89d953b45f79aacdad2fb25ee9535e
Jingyi Zhang <jz3533@columbia.edu>
2023-01-19 09:28:26 -0500

    commented out superuser

--- app1/views.py
+++ app1/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+
+from app1 import support_functions
+from app1.models import *
+
+
 def home(request):
     data=dict()
     import datetime
@@ -6,3 +11,27 @@ def home(request):
     # data['xy']=xy
     return render(request,"home.html",context=data)
 # Create your views here.
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+def maintenance(request):
+    data = dict()
+    try:
+        choice = request.GET['selection']
+        if choice == "currencies":
+            support_functions.add_currencies(support_functions.get_currency_list())
+            c_list = Currency.objects.all()
+            print("Got c_list",len(c_list))
+            data['currencies'] = c_list
+            return HttpResponseRedirect(reverse('currencies'))
+    except:
+        pass
+    return render(request,"maintenance.html",context=data)
+# def maintenance(request):
+#     data = dict()
+#     return render(request,"maintenance.html",context=data)
+def view_currencies(request):
+    data = dict()
+    c_list = Currency.objects.all()
+    data['currencies'] = c_list
+    return render(request,'currencies.html',context=data)
